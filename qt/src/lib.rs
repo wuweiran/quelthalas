323035e83949f74eb890fd57285c411228a46eb7
wuweiran <wwrnfls@126.com>
2024-03-15 22:47:52 +0800

    Fix initial commit

--- qt/src/lib.rs
+++ qt/src/lib.rs
@@ -1,1 +1,36 @@
+extern crate self as qt;
+
+use windows::core::Result;
+use windows::Win32::Foundation::HWND;
+use windows::Win32::UI::HiDpi::GetDpiForWindow;
+
+use qt::theme::Tokens;
+
+pub struct MouseEvent {
+    on_click: fn(),
+}
+
+impl Default for MouseEvent {
+    fn default() -> Self {
+        MouseEvent { on_click: || {} }
+    }
+}
+
+pub struct QT {
+    tokens: Tokens,
+}
+
+impl QT {
+    pub fn new() -> Result<Self> {
+        Ok(QT {
+            tokens: Tokens::web_light(),
+        })
+    }
+}
+
+pub(crate) fn get_scaling_factor(window: &HWND) -> f32 {
+    unsafe { GetDpiForWindow(*window) as f32 / 96.0f32 }
+}
+
+pub mod component;
 mod theme;
