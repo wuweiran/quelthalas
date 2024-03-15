40e5ed7080b4ad75f18f7cea9340fbb1461d1b48
wuweiran <wwrnfls@126.com>
2024-03-15 22:37:55 +0800

    Initial commit

---
+++ qt/src/theme.rs
@@ -0,0 +1,7 @@
+use windows::Win32::Graphics::Direct2D::Common::D2D1_COLOR_F;
+
+struct Tokens {
+    color_neutral_background1: D2D1_COLOR_F,
+    color_neutral_foreground1: D2D1_COLOR_F,
+    stroke_width_
+}
