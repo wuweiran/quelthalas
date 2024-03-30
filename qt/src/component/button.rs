d2018fdbd3db394b2cced6081e94b0ba40eace0c
wuweiran <wwrnfls@126.com>
2024-03-30 11:11:23 +0800

    Button: use `CS_CLASSDC` instead of `CS_OWNDC`

--- qt/src/component/button.rs
+++ qt/src/component/button.rs
@@ -176,7 +176,7 @@ impl QT {
             let window_class = WNDCLASSEXW {
                 cbSize: size_of::<WNDCLASSEXW>() as u32,
                 lpszClassName: class_name,
-                style: CS_OWNDC,
+                style: CS_CLASSDC,
                 lpfnWndProc: Some(window_proc),
                 hCursor: LoadCursorW(None, IDC_ARROW)?,
                 ..Default::default()
