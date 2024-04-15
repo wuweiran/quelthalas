038dea0b60666c96691de594a1926826c0e01b43
Weiran Wu <weiranwu@microsoft.com>
2024-04-15 10:29:10 +0800

    Menu: reconstruct Menu scaffold

--- qt/src/component/menu.rs
+++ qt/src/component/menu.rs
@@ -1,10 +1,12 @@
 use std::mem::size_of;
+use std::rc::Rc;
 
 use windows::core::*;
 use windows::Win32::Foundation::{
-    ERROR_INVALID_WINDOW_HANDLE, FALSE, HINSTANCE, HWND, LPARAM, LRESULT, TRUE, WPARAM,
+    ERROR_INVALID_WINDOW_HANDLE, FALSE, HINSTANCE, HWND, LPARAM, LRESULT, POINT, TRUE, WPARAM,
 };
 use windows::Win32::Graphics::Gdi::{BeginPaint, EndPaint, HDC, PAINTSTRUCT};
+use windows::Win32::UI::Input::KeyboardAndMouse::SetCapture;
 use windows::Win32::UI::WindowsAndMessaging::*;
 
 use crate::QT;
@@ -17,20 +19,45 @@ pub enum MenuInfo {
     MenuDivider,
 }
 
-struct MenuItem {
-    info: MenuInfo,
+pub enum MenuItem {
+    MenuItem { sub_menu: Rc<Menu>, text: PCWSTR },
+    MenuDivider,
 }
 
 pub struct Menu {
-    qt: QT,
     items: Vec<MenuItem>,
+}
+
+pub struct Context {
+    qt: QT,
+    menu: Rc<Menu>,
     owning_window: HWND,
+}
+
+fn convert_menu_info_list_to_menu(menu_info_list: Vec<MenuInfo>) -> Menu {
+    let items = menu_info_list
+        .into_iter()
+        .map(|menu_info| match menu_info {
+            MenuInfo::MenuItem {
+                sub_menu_list,
+                text,
+            } => {
+                let sub_menu = convert_menu_info_list_to_menu(sub_menu_list);
+                MenuItem::MenuItem {
+                    sub_menu: Rc::new(sub_menu),
+                    text,
+                }
+            }
+            MenuInfo::MenuDivider => MenuItem::MenuDivider,
+        })
+        .collect();
+    Menu { items }
 }
 
 impl QT {
     pub unsafe fn open_menu(
         &self,
-        parent_window: &HWND,
+        parent_window: HWND,
         menu_list: Vec<MenuInfo>,
         x: i32,
         y: i32,
@@ -45,17 +72,15 @@ impl QT {
             ..Default::default()
         };
         RegisterClassExW(&window_class);
-        if IsWindow(parent_window).into() == false {
+        let is_parent_window_valid : bool = IsWindow(parent_window).into();
+        if !is_parent_window_valid {
             return Err(Error::from(ERROR_INVALID_WINDOW_HANDLE));
         }
-        let items = menu_list
-            .into_iter()
-            .map(|menu_info| MenuItem { info: menu_info })
-            .collect();
-        let boxed = Box::new(Menu {
+        let menu = Rc::new(convert_menu_info_list_to_menu(menu_list));
+        let boxed = Box::new(Context {
             qt: self.clone(),
-            items,
-            owning_window: *parent_window,
+            menu: menu.clone(),
+            owning_window: parent_window,
         });
         let window = CreateWindowExW(
             WINDOW_EX_STYLE::default(),
@@ -69,11 +94,11 @@ impl QT {
             parent_window,
             None,
             HINSTANCE(GetWindowLongPtrW(parent_window, GWLP_HINSTANCE)),
-            Some(Box::<Menu>::into_raw(boxed) as _),
+            Some(Box::<Context>::into_raw(boxed) as _),
         );
-        init_tracking(*parent_window)?;
-        track_menu(window, 0, 0, *parent_window)?;
-        exit_tracking(*parent_window)?;
+        init_tracking(parent_window)?;
+        track_menu(window, menu.clone(), 0, 0, parent_window)
+            .and(exit_tracking(parent_window))?;
         Ok(())
     }
 }
@@ -81,13 +106,13 @@ impl QT {
 unsafe fn init_tracking(owning_window: HWND) -> Result<()> {
     HideCaret(None)?;
     SendMessageW(
-        SendMessageW,
+        owning_window,
         WM_ENTERMENULOOP,
         WPARAM(TRUE.0 as usize),
         LPARAM(0),
     );
     SendMessageW(
-        SendMessageW,
+        owning_window,
         WM_SETCURSOR,
         WPARAM(owning_window.0 as usize),
         LPARAM(HTCAPTION as isize),
@@ -95,13 +120,68 @@ unsafe fn init_tracking(owning_window: HWND) -> Result<()> {
     Ok(())
 }
 
-unsafe fn track_menu(window: HWND, x: i32, y: i32, owning_window: HWND) -> Result<()> {
+struct Tracker {
+    current_menu: Rc<Menu>,
+    top_menu: Rc<Menu>,
+    owning_window: HWND,
+    point: POINT,
+}
+
+unsafe fn track_menu(
+    window: HWND,
+    menu: Rc<Menu>,
+    x: i32,
+    y: i32,
+    owning_window: HWND,
+) -> Result<()> {
+    SetCapture(window);
+    let mut mt = Tracker {
+        current_menu: menu.clone(),
+        top_menu: menu.clone(),
+        owning_window,
+        point: Default::default(),
+    };
+    let mut exit_menu = false;
+    let mut enter_idle_sent = false;
+    while !exit_menu {
+        let mut msg = MSG::default();
+        loop {
+            if PeekMessageW(&mut msg, None, 0, 0, PM_NOREMOVE).into() {
+                if CallMsgFilterW(&msg, MSGF_MENU as i32).into() {
+                    break;
+                }
+                PeekMessageW(&mut msg, None, msg.message, msg.message, PM_REMOVE);
+            } else {
+                if !enter_idle_sent {
+                    enter_idle_sent = true;
+                    SendMessageW(
+                        owning_window,
+                        WM_ENTERIDLE,
+                        WPARAM(MSGF_MENU as usize),
+                        LPARAM(window.0),
+                    );
+                    MsgWaitForMultipleObjectsEx(
+                        None,
+                        0xffffffff,
+                        QS_ALLINPUT,
+                        MSG_WAIT_FOR_MULTIPLE_OBJECTS_EX_FLAGS::default(),
+                    );
+                }
+            }
+        }
+
+        if msg.message == WM_CANCELMODE {
+            exit_menu = true;
+            PeekMessageW(&mut msg, None, msg.message, msg.message, PM_REMOVE);
+            break;
+        }
+    }
     Ok(())
 }
 
 unsafe fn exit_tracking(owning_window: HWND) -> Result<()> {
     SendMessageW(
-        SendMessageW,
+        owning_window,
         WM_EXITMENULOOP,
         WPARAM(TRUE.0 as usize),
         LPARAM(0),
@@ -112,7 +192,7 @@ unsafe fn exit_tracking(owning_window: HWND) -> Result<()> {
 
 unsafe fn show_popup(
     owning_window: HWND,
-    menu_list: &Vec<MenuItem>,
+    menu_list: &Menu,
     id: usize,
     x: i32,
     y: i32,
@@ -135,11 +215,23 @@ extern "system" fn window_proc(
     match message {
         WM_CREATE => unsafe {
             let cs = l_param.0 as *const CREATESTRUCTW;
-            let raw = (*cs).lpCreateParams as *mut Menu;
-            let menu = Box::<Menu>::from_raw(raw);
-            match show_popup(menu.owning_window, &menu.items, 0, (*cs).x, (*cs).y, 0, 0) {
+            let raw = (*cs).lpCreateParams as *mut Context;
+            let context = Box::<Context>::from_raw(raw);
+            match show_popup(
+                context.owning_window,
+                context.menu.as_ref(),
+                0,
+                (*cs).x,
+                (*cs).y,
+                0,
+                0,
+            ) {
                 Ok(_) => {
-                    SetWindowLongPtrW(window, GWLP_USERDATA, Box::<Menu>::into_raw(menu) as _);
+                    SetWindowLongPtrW(
+                        window,
+                        GWLP_USERDATA,
+                        Box::<Context>::into_raw(context) as _,
+                    );
                     LRESULT(TRUE.0 as isize)
                 }
                 Err(_) => LRESULT(FALSE.0 as isize),
